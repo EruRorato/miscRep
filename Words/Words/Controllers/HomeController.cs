b08a95b946279359a74f2ebee40bf85658a999e0
Eru Rorato <sadwolfy@gmail.com>
2016-06-21 21:28:16 +0500

    Words Game MVC
    words game MVC

---
+++ Words/Words/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Newtonsoft.Json.Linq;
+using System.IO;
+
+namespace Words.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        
+        [HttpGet]
+        public ActionResult getWord()
+        {
+            //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+           //My function to get URL and WORD. Replace it with SQL query.
+            var fileName = Server.MapPath(Url.Content("~/App_Data/wordsDB.txt"));
+            var line = "";
+            Random r = new Random();
+            var rndNum = r.Next(1, System.IO.File.ReadLines(fileName).Count());
+
+            line = System.IO.File.ReadLines(fileName).Skip(rndNum).Take(1).First(); ;
+           
+            String[] substrings = line.Split(';');
+            //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+            var newWord = substrings[0]; //set New Word 
+            var newLink = substrings[1]; //set New Link
+            var data = new Words.Models.DataModel()
+            {
+                Word = newWord,
+                Link = newLink
+            };
+            var jWord = Json(data, JsonRequestBehavior.AllowGet);
+            return jWord;
+        }
+    }
+}
---
+++ Words/Words/Models/DataModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Words.Models
+{
+    public class DataModel
+    {
+        public string Word { get; set; }
+        public string Link{get;set;}
+    }
+}
